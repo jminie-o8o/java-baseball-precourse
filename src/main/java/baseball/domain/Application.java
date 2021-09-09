99369e0681a88a0b1411c5b1abd113dd874a04a0
hwj <jminie@naver.com>
2021-09-09 19:43:04 +0900

    서로 다른 임의의 수 3개를 생성하는 기능 구현

--- src/main/java/baseball/domain/Application.java
+++ src/main/java/baseball/domain/Application.java
@@ -9,6 +9,7 @@ package baseball.domain;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        NumberGenerator numberGenerator = new NumberGenerator();
+        System.out.println(numberGenerator.createRandomNumbers());
     }
 }
